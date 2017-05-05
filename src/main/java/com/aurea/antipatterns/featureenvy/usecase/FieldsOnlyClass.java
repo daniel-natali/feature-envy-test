57b4385aec58d3c102cd16327f10ebd4ca723b37
daniel.natali <daniel.natali@aurea.com>
2017-05-04 23:38:05 -0300

    Initial version

---
+++ src/main/java/com/aurea/antipatterns/featureenvy/usecase/FieldsOnlyClass.java
@@ -0,0 +1,13 @@
+package com.aurea.antipatterns.featureenvy.usecase;
+
+//CHECKSTYLE:OFF
+/**
+ * This class must be ignored as is a data class
+ * Created by valb3r on 03.06.16.
+ */
+//CHECKSTYLE:OFF
+public class FieldsOnlyClass {
+    public int refIntVal;
+    public String refStrVal;
+    public ReferenceClass refRefVal;
+}
